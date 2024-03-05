81e00c61743a7f94f81c5371e08b33d065a3a94e
Dmytro <dima50066@gmail.com>
2024-03-05 19:13:47 +0100

    create task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,11 @@
 function getElementWidth(content, padding, border) {
-  return;
+  return (
+    Number.parseInt(content) +
+    (Number.parseInt(padding) +
+      Number.parseInt(padding) +
+      Number.parseFloat(border) +
+      Number.parseFloat(border))
+  );
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
